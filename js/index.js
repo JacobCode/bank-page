17e5e2f29dbf0f38d2a75aa7c3812dbe88a7df65
Jacob Carver <jacxbcarver@gmail.com>
2019-08-12 00:18:02 -0600

    setup initial layout, made responsive, added menu

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.querySelector('.menu-toggler').addEventListener('click', function() {
+	this.classList.toggle('open');
+	document.querySelector('.menu').classList.toggle('open');
+})
+
+document.addEventListener('scroll', function() {
+	if (document.querySelector('.menu').classList[1] !== undefined && document.querySelector('.menu-toggler').classList[1] !== undefined) {
+		document.querySelector('.menu').classList.remove('open');
+		document.querySelector('.menu-toggler').classList.remove('open');
+	}
+})
